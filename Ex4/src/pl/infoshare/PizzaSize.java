0b841cf0393f87898ca1627ba65da3652b3473dc
Mateusz Lesisz <m.lesisz97@wp.pl>
2021-10-15 17:30:24 +0200

    almost done vol 2

--- Ex4/src/pl/infoshare/PizzaSize.java
+++ Ex4/src/pl/infoshare/PizzaSize.java
@@ -1,7 +1,19 @@
 package pl.infoshare;
 
 public enum PizzaSize {
-    MAŁA,
-    ŚREDNIA,
-    DUŻA;
+    MAŁA {
+        public String toString() {
+            return "mała";
+        }
+    },
+    ŚREDNIA{
+        public String toString() {
+            return"średnia";
+        }
+    },
+    DUŻA {
+        public String toString() {
+            return"duża";
+        }
+    };
 }
